a9944694951883df066b39ccd133ca0676c64894
tianshuixiafei <tianshuixiafei@163.com>
2019-08-31 19:59:32 +0800

    commit my first project to test-demo

--- src/main/java/com/robin/test/Player.java
+++ src/main/java/com/robin/test/Player.java
@@ -1,49 +1,17 @@
 package com.robin.test;
 
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@Data
 public class Player {
+
     private String name;
     private String team;
     private Integer age;
     private Integer score;
 
 
-    public Player(String name, String team, Integer age, Integer score) {
-        this.name = name;
-        this.team = team;
-        this.age = age;
-        this.score = score;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTeam() {
-        return team;
-    }
-
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Integer getScore() {
-        return score;
-    }
-
-    public void setScore(Integer score) {
-        this.score = score;
-    }
 }
